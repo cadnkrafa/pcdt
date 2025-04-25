a71da60e3e2e2d9d3a021a989048e277a526ba0d
cadnkrafa <cad.nkrafa@gmail.com>
2025-04-25 13:38:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const lineWebhook = require('./line-webhook');
+require('dotenv').config();
 
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/webhook', lineWebhook);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
